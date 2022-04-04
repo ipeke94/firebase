72f3c06d9f2339724ff92704bc291e266423ae88
ipeke94 <ipekerbakis@gmail.com>
2022-04-04 18:12:59 +0200

    Updates

---
+++ precache-manifest.be1ad59e80991f65812dfc4004eaaa09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "789ec1d5b3c02088ab48",
+    "url": "/firebase/static/css/main.cb530f36.chunk.css"
+  },
+  {
+    "revision": "789ec1d5b3c02088ab48",
+    "url": "/firebase/static/js/main.0afcf43c.chunk.js"
+  },
+  {
+    "revision": "7f2b17a1a87a6052eb18",
+    "url": "/firebase/static/js/runtime~main.e7591a97.js"
+  },
+  {
+    "revision": "916a4a8c32ba09c4386d",
+    "url": "/firebase/static/js/2.5eae5d22.chunk.js"
+  },
+  {
+    "revision": "281cb511bd62638d8b50f81648acbf60",
+    "url": "/firebase/index.html"
+  }
+];
